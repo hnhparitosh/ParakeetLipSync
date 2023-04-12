a54ef8d7afd855bc6b7927a722ddcca37d40e780
Paritosh Dahiya <dahiyaparitosh@gmail.com>
2023-04-12 18:13:19 +0530

    update BasicView

--- LipsyncFrame.py
+++ LipsyncFrame.py
@@ -1,20 +1,37 @@
-from PySide6.QtWidgets import QMainWindow, QPushButton, QApplication, QGridLayout, QWidget
-from WaveformView import WaveformView
-from MouthView import MouthView
-from DialogueView import DialogueView
+from PySide6.QtWidgets import QMainWindow, QApplication, QGridLayout, QWidget, QLabel, QVBoxLayout
+from PySide6.QtGui import QAction
+from PySide6.QtCore import Qt
+
+# from WaveformView import WaveformView
+# from MouthView import MouthView
+# from DialogueView import DialogueView
 import sys
 
 class LipsyncFame(QMainWindow):
     def __init__(self) :
         super().__init__()
         self.setWindowTitle("Parakeet Lipsync")
-        
-        layout = QGridLayout()
-        layout.addWidget(WaveformView,0,0)
-        layout.addWidget(DialogueView,1,0)
-        layout.addWidget(MouthView,1,1)
+        self.resize(500, 500)
 
-        widget = QWidget()
-        widget.setLayout(layout)
+        self.menuBar = self.menuBar()
 
-        self.setCentralWidget(widget)
+        # adding menus
+        fileMenu = self.menuBar.addMenu('File')
+
+        # defining actions
+        open_action = QAction('Open file', self)
+        open_action.setShortcut('Ctrl+O')
+        open_action.triggered.connect(  )
+
+        # defining widgets
+
+
+        # layout = QVBoxLayout()
+        # layout.addWidget(WaveformView,0,0)
+        # layout.addWidget(DialogueView,1,0)
+        # layout.addWidget(MouthView,1,1)
+
+        # widget = QWidget()
+        # widget.setLayout(layout)
+
+        # self.setCentralWidget(widget)
---
+++ MouthView.py
@@ -0,0 +1,1 @@
+# this view will be implemented later
